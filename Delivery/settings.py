d8d2d2913b747ff31a6d69a8f77ee6ca41bff9bf
peachgoma6601 <104054433+peachgoma6601@users.noreply.github.com>
2022-06-08 17:47:31 +0530

    Update settings.py

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -128,3 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
+heroku config:set DISABLE_COLLECTSTATIC=1
